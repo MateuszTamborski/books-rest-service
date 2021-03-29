740404b82b4a40305f4a16295e894b247a8e6f17
MateuszTamborski <50679915+MateuszTamborski@users.noreply.github.com>
2021-03-30 01:56:08 +0200

    [ADD] Endpoint book by isbn

--- src/main/java/com/example/booksrestservice/controller/BookController.java
+++ src/main/java/com/example/booksrestservice/controller/BookController.java
@@ -3,18 +3,25 @@ package com.example.booksrestservice.controller;
 import com.example.booksrestservice.model.Book;
 import com.example.booksrestservice.service.BookService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService = new BookService();
 
-
-    @GetMapping("/books") //for testing deserialization purposes
+    @GetMapping //for testing deserialization purposes
     public List<Book> findAllBooks(){
-        return bookService.findBooks();
+        return bookService.findAll();
+    }
+
+    @GetMapping(value = "/{isbn}")
+    public Book findByIsbn(@PathVariable("isbn") String isbn){
+        return bookService.findByIsbn(isbn);
     }
 
 }
--- src/main/java/com/example/booksrestservice/service/BookService.java
+++ src/main/java/com/example/booksrestservice/service/BookService.java
@@ -6,10 +6,16 @@ import com.example.booksrestservice.model.Book;
 import java.util.List;
 
 public class BookService {
+
     private final BooksMapper booksMapper = new BooksMapper();
 
-    public List<Book> findBooks(){
+    public List<Book> findAll(){
         List<Book> books = booksMapper.mapObjects();
         return books;
+    }
+
+    public Book findByIsbn(String isbn){
+        List<Book> books = booksMapper.mapObjects();
+        return books.stream().filter(f->f.getIsbn().equals(isbn)).findFirst().get();
     }
 }
